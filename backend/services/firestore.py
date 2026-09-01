5508754986df9f28b7bc5e0b09cad0131f7de38e
deep-gotecha <dgotecha99@gmail.com>
2026-09-01 16:04:21 +0530

    deployment process

--- backend/services/firestore.py
+++ backend/services/firestore.py
@@ -1,10 +1,28 @@
+import os
+import json
+
 import firebase_admin
-from firebase_admin import credentials, firestore
+
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(
-        "credentials/serviceAccountKey.json"
+
+    firebase_credentials = json.loads(
+        os.getenv("FIREBASE_CREDENTIALS")
     )
+
+    cred = credentials.Certificate(
+        firebase_credentials
+    )
+
     firebase_admin.initialize_app(cred)
+
 
 db = firestore.client()
